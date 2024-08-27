1f759082b408ebb3eb47c290d22ef10278219fcf
Williams313 <williamslambert313@gmail.com>
2024-08-27 11:32:18 -0700

    commit

---
+++ IntegerAverage.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IntegerAverage {
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+
+System.out.print("Enter three numbers: ");
+ double number1 = input.nextDouble();
+ double number2 = input.nextDouble();
+ double number3 = input.nextDouble();
+
+ double average = (number1 + number2 + number3) / 3;
+
+ System.out.printf("The average of numbers " + %d );
+} 
+}
+ ;         
